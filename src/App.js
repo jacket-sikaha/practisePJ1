94d5b01989760b337b3db6bf6158a0f0503a7a97
sikaha <77327970+jacket-sikaha@users.noreply.github.com>
2022-08-15 19:40:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react";
 function App() {
   const [result, setResult] = useState(0);
   // let calculate = useRef([]);
-  let numArr = useRef([]);
+  let pointStatus = useRef(false);
   // let startZeroFlag = useRef(true);
   let changeCalculate = useRef(false);
   const [calculate, setCalculate] = useState([]);
@@ -13,16 +13,13 @@ function App() {
   const [presentVal, setPresentVal] = useState("0");
 
   const getValue = (value) => {
-    console.log("numArr.current", numArr.current);
     //change
-    if (value === "change") {
-      return;
-    }
-
     //重制操作
-    if (value === "C") {
+    if (value === "C" || value === "change") {
       setPresentVal("0");
       setResult(0);
+      changeCalculate.current = false;
+      pointStatus.current = changeCalculate.current;
       return;
     }
 
@@ -34,8 +31,27 @@ function App() {
           return "0";
         } else {
           let res = oldVal.slice(0, oldVal.length - 1);
-          //删除到计算符会变成切换模式
-          changeCalculate.current = isNaN(res.slice(-1));
+          if (res.slice(-1) === ".") {
+            //直接是小数点
+            pointStatus.current = true;
+            changeCalculate.current = false;
+          } else if (isNaN(res.slice(-1))) {
+            //删除到计算符  会变成切换模式
+            changeCalculate.current = true;
+            pointStatus.current = !changeCalculate.current;
+          } else {
+            //删除到num---小数和整数需要另外确定
+            changeCalculate.current = false;
+            res
+              // .match(/([1-9]\d*\.?\d*)|(0\.?\d*)/g)
+              // 考虑了。x  和  x。 两种特殊输入
+              //正则表达式获取最后一个数字形式，以此去调整pointStatus状态
+              .match(/([1-9]\d*\.?\d*)|(0\.?\d*)|(\.\d+)|(\d+\.$)/g)
+              .slice(-1)[0]
+              .indexOf(".") >= 0
+              ? (pointStatus.current = true)
+              : (pointStatus.current = false);
+          }
           return res;
         }
       });
@@ -58,17 +74,25 @@ function App() {
         } else {
           setPresentVal((oldVal) => oldVal + value);
           changeCalculate.current = true;
+          pointStatus.current = false;
         }
         // calculate.push(value);
         // numArr.push(parseFloat(presentVal));
       } else {
-        console.log(". %");
+        //pointStatus限制。的重复添加
+        if (value === "." && !pointStatus.current) {
+          setPresentVal((oldVal) => oldVal + value);
+          pointStatus.current = true;
+          changeCalculate.current = false;
+          console.log(".");
+        }
       }
     } else {
       //add number
       let lastNum = parseFloat(presentVal.match(/\d+/g).slice(-1)[0]);
       //靠右数字不为0 或者 计算字符还未输入，或符合（）原则 就直接加
-      if (lastNum !== 0 || changeCalculate.current) {
+      //添加。后数字变成小数状态，没有添加限制
+      if (lastNum !== 0 || changeCalculate.current || pointStatus.current) {
         setPresentVal((oldVal) => {
           return oldVal + value;
         });
@@ -78,7 +102,7 @@ function App() {
           setPresentVal(presentVal.slice(0, presentVal.length - 1) + value);
         }
       }
-      //可以添加计算符号标记
+      //添加计算符号标记
       changeCalculate.current = false;
     }
   };
@@ -106,6 +130,7 @@ function App() {
       }
     }
   };
+
   return (
     <div className="t">
       <Main presentVal={presentVal} result={result}></Main>
